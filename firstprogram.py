a0fa9155cd81ca79a188911536a12be1ba019b5c
AbdullahNadeem99 <nadeemabdullah819@gmail.com>
2026-02-23 22:38:32 +0500

    Python learning progress

---
+++ firstprogram.py
@@ -0,0 +1,29 @@
+print("hello world")
+a=20
+b=10
+print(a+b)
+sum=a+b
+print(sum)
+
+
+#koi bhi string kisi numeric value 
+# se multiply hoti hai to woh string ka content utni dafa print ho jata hai eg
+a,b=2,3
+contentt="a"
+print(2*contentt*3)
+
+
+#agar integer aur float ko add karain to result hamesha flaot main aeyga
+#divison operater : / hamesha float main hi answer dega
+#integer divison : // isse hoti hai yeh int main answer deta hia bass likhta float main hai
+
+namee=input("name: ") 
+age=int(input("age:"))
+print("my name is :",namee, "my age is : ",age)
+
+#agar string input leni ho to phir hamain data type dene ki zarrurat nai hai
+#  but agar int float wagera input lene ho to phir hamain sath data type 
+# mention kanre ki zarrurat hai
+
+# ** ka matlab hota hai power means a**b means a to the power of b
+
---
+++ if-else-eg.py
@@ -0,0 +1,11 @@
+#if elif eg
+
+colour=input("color: ")
+if(colour=="green"):
+    print("go")
+elif(colour=="red"):
+    print("stop")
+elif(colour=="yellow"):
+    print("look")
+else:
+    print("broken")
