b2ad0ea99eed82f9571f45d3252b65e8018b9593
AbdullahNadeem99 <nadeemabdullah819@gmail.com>
2026-02-24 02:36:28 +0500

    simple basic practice questions

---
+++ pracquestion.py
@@ -0,0 +1,14 @@
+#Q1
+#area of square eg question:
+length=int((input("enter length of the square: ")))
+sum=length*length
+print("area of square: ",sum)
+
+
+
+#Q2
+#input 2 numbers and print true if the first num is greater then equal to second
+
+a=int(input("enter first num: "))
+b=int(input("enter sec num: "))
+print(a>=b)
