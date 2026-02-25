fb67737ae7f9da1b1839aace12d9f42bb817439e
AbdullahNadeem99 <nadeemabdullah819@gmail.com>
2026-02-26 03:55:16 +0500

    basic strings questions

---
+++ strings-with-eg.py
@@ -0,0 +1,16 @@
+str1="my name is abdullah"
+str2="i live in lahore"
+print(str1+str2)#concatination
+print(len(str1))#length of a string. (length ke andar spaces bhi count ho rahi hoti hain)
+
+
+#slicing(megative numbers se bhi ho jati. last index is -1)
+str="my name is abdulah"
+print(str[1:8])#str[starting index:ending index](ending index is not included).
+
+ 
+strr=input("enter the first name: ")
+print(len(strr))
+
+
+
