41c54dcc25b6120f88a1aa6b6b1be5b1f23b0bcc
AbdullahNadeem99 <nadeemabdullah819@gmail.com>
2026-03-01 23:11:42 +0500

    basic dictionary and set examples

---
+++ dict-and-set.py
@@ -0,0 +1,18 @@
+words={
+    "table": ["a peice of furniture","list of facts and figures"],
+    "cat":"a small animal"
+}
+print(words)
+
+
+subject={"python","java","c++","python","javascript","java","python","java","c++","c"}
+print(len(subject))
+
+
+
+user={}
+x=int(input("enter maths marks: "))
+user.update({"maths": x})
+y=input("enter physics maths: ")
+user.update({"physics": y})
+print(user)
